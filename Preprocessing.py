cd3df673799c7764a96e9f65f9a9092b523c1d09
Hao Huang <rikka@Haos-MacBook-Air.local>
2025-09-03 16:05:34 -0400

    preprocessing and first 3 visualizations

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,1 +1,30 @@
-#preprocessing
+from config import *
+
+# age  sex  cp  trestbps  chol  fbs  restecg  thalach  exang  oldpeak  slope  ca  thal  target
+df = pd.read_csv('PACSMV-103021-Heart_Disease/Heart_disease_statlog.csv')
+
+
+# extend abbreviation of the attributes
+df.columns = ['age', 'sex', 'chest_pain_type', 'resting_blood_pressure', 'cholesterol',
+              'fasting_blood_sugar', 'rest_ecg', 'max_heart_rate_achieved',
+              'exercise_induced_angina', 'st_depression', 'st_slope','ca','thalassemia','target']
+print(df.columns)
+
+
+# convert number features to categorical features
+cp_mapping = {0: 'typical angina', 1: 'atypical angina', 2: 'non-angina pain', 3: 'asymptomatic'}
+ecg_mapping = {0: 'normal', 1: 'Abnormality in ST-T wave', 2: 'left ventricular hypertrophy'}
+slope_mapping = {0: 'upsloping', 1: 'flat', 2: 'downsloping'}
+thal_mapping = {0: 'null', 1: 'fixed defect', 2: 'normal blood flow', 3: 'reversible defect'}
+sex_mapping = {1: 'male', 0: 'female'}
+
+df['chest_pain_type'] = df['chest_pain_type'].map(cp_mapping)
+df['rest_ecg'] = df['rest_ecg'].map(ecg_mapping)
+df['st_slope'] = df['st_slope'].map(slope_mapping)
+df['thalassemia'] = df['thalassemia'].map(thal_mapping)
+df['sex'] = df['sex'].map(sex_mapping)
+print(df.head())
+
+# check missing value
+print(df.isna().sum())
+
---
+++ config.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+
+
+
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (10, 8)
+plt.rcParams['font.size'] = 12
